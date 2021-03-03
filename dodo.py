66f1e80123af08d0bd41756f2f3f2d0107d9c280
Nicholas Bollweg <nick.bollweg@gmail.com>
2021-03-02 23:11:17 -0500

    clean up linking

--- dodo.py
+++ dodo.py
@@ -5,8 +5,10 @@ https://github.com/jupyterlab/lumino@09aec10
 # change the urls above to link different jupyter references.
 
 
+import os
 import pathlib
-import doit
+import doit.tools
+import json
 
 DOIT_CONFIG = {
     "backend": "sqlite3",
@@ -14,9 +16,23 @@ DOIT_CONFIG = {
     "par_type": "thread",
 }
 
+os.environ.update(
+    PYTHONUNBUFFERED="1",
+    PYTHONIOENCODING="utf-8",
+    PIP_NO_DEPS="1",
+    PIP_NO_BUILD_ISOLATION="1",
+    PIP_DISABLE_PIP_VERSION_CHECK="1",
+    PIP_IGNORE_INSTALLED="1"
+)
+
 HERE = pathlib.Path(__file__).parent
 
-repos = list(filter(bool, __doc__.splitlines()))
+# don't pollute the global state
+LINKS = (HERE / ".yarn-links").resolve()
+YARN = ["yarn", "--link-folder", LINKS]
+PIP = ["python", "-m", "pip"]
+
+URLS = list(filter(bool, __doc__.splitlines()))
 
 
 def task_lint():
@@ -31,62 +47,82 @@ def task_lint():
 # add targets to the docstring to include in the dev build.
 def task_clone():
     """clone all the repos defined in the doc string"""
-    for repo in repos:
-        repo, _, branch = repo.rpartition("@")
-        path = pathlib.Path(repo.rpartition("/")[2])
+    for url in URLS:
+        path = url_to_path(url)
         yield dict(
-            name=f"clone_{repo}",
-            actions=[] if path.exists() else [f"git clone --depth 1 {repo}"],
+            name=path.name,
+            actions=[] if path.exists() else [do("git", "clone", "--depth=1", url)],
             targets=[path / "package.json"]
         )
 
-def task_build():
+
+def task_setup():
     """ensure a working build of live development builds"""
-    for dep in [repo_to_path(x) / "package.json" for x in repos]:
+    for repo in map(url_to_path, URLS):
         yield dict(
-            name=f"install_{dep}",
-            file_dep=[dep],
-            actions=[
-                f"""cd {dep.parent} && jlpm --prefer-offline --ignore-optional"""
-            ],
-            targets=[dep.parent / "node_modules/.yarn-integrity"]
-        )
-        yield dict(
-            name=f"build_{dep}",
-            file_dep=[dep.parent / "node_modules/.yarn-integrity"],
-            actions=[
-                f"""cd {dep.parent} && jlpm build"""
-            ],
-            targets=list(dep.parent.rglob("packages/*/lib/*.js"))
+            name=f"{repo.name}:yarn",
+            file_dep=[repo / "package.json"],
+            actions=[do(*YARN, cwd=repo)],
+            targets=yarn_integrity(repo)
         )
 
-def task_link_lumino():
-    """jupter labextension link changes across the repos"""
-    # find the changes in the repos relative to master.
+        setup_py = repo / "setup.py"
+
+        if setup_py.exists():
+            yield dict(
+                name=f"{repo.name}:pip",
+                file_dep=yarn_integrity(repo),
+                actions=[
+                    do(*PIP, "uninstall", "-y", repo.name, cwd=repo),
+                    do(*PIP, "install", "-e", ".", cwd=repo),
+                    do(*PIP, "check")
+                ]
+            )
+
+        yield dict(
+            name=f"{repo.name}:build",
+            file_dep=yarn_integrity(repo),
+            actions=[
+                do(*YARN, "build", cwd=repo)
+            ],
+            targets=list(repo.glob("packages/*/lib/*.js")),
+            **(
+                dict(task_dep=[f"setup:{repo.name}:pip"]) if setup_py.exists() else {}
+            )
+        )
+
+
+def task_link():
+    """link yarn packages across the repos"""
     # go to the direction and links the packages.
-    for pkg in pathlib.Path("lumino/packages").glob("*/package.json"):
+    lumino = [url_to_path(u) for u in URLS if "jupyterlab/lumino" in u][0]
+    lab = [url_to_path(u) for u in URLS if "jupyterlab/jupyterlab" in u][0]
+
+    for pkg_json in lumino.glob("packages/*/package.json"):
+        pkg = pkg_json.parent
+        pkg_data = json.loads(pkg_json.read_text(encoding="utf-8"))
+        pkg_name = pkg_data["name"]
+        out_link = LINKS / pkg_data["name"] / "package.json"
+        in_link = lab / f"node_modules/{pkg_name}/package.json"
         yield dict(
-            name=f"link_lumino_{pkg.parent.name}",
-            file_dep=[
-                pkg,
-                pkg.parent.parent.parent / "node_modules/.yarn-integrity"
+            name=pkg_name,
+            file_dep=[*yarn_integrity(lumino), *yarn_integrity(lab), pkg_json],
+            actions=[
+                (doit.tools.create_folder, [LINKS]),
+                do(*YARN, "link", cwd=pkg)
             ],
-            actions=f"""
-                cd lumino/packages/{pkg.parent.name} \
-                && jlpm link
-
-                """.strip().splitlines(),#&& touch ../../build/link.lumino.{pkg.parent.name}.ok
-            targets=[]# [f"build/link.lumino.{pkg.parent.name}.ok"]
+            targets=[out_link]
         )
-        yield dict(
-            name=f"link_lab_{pkg.parent.name}",
-            file_dep=[],#[f"build/link.lumino.{pkg.parent.name}.ok"],
-            actions=f"""
-                cd jupyterlab \
-                && jlpm link @lumino/{pkg.parent.name}
 
-                """.strip().splitlines(), #&& touch ../build/link.lab.{pkg.parent.name}.ok
-            targets=[f"build/link.lab.{pkg.parent.name}.ok"]
+        yield dict(
+            name=f"lab:{pkg_name}",
+            uptodate=[doit.tools.config_changed({
+                pkg_name: (
+                    in_link.exists() and in_link.resolve() == pkg_json.resolve()
+                )
+            })],
+            file_dep=[out_link],
+            actions=[do(*YARN, "link", pkg_name, cwd=lab)]
         )
 
 def task_config():
@@ -110,6 +146,14 @@ def task_config():
 
 # utilities
 
-def repo_to_path(x):
+def url_to_path(x):
     """extract the local checkout name"""
     return pathlib.Path(x.rpartition("@")[0].rpartition("/")[2])
+
+def do(*args, cwd=HERE, **kwargs):
+    """wrap a CmdAction for consistency"""
+    return doit.tools.CmdAction(list(args), shell=False, cwd=str(pathlib.Path(cwd)))
+
+def yarn_integrity(repo):
+    """the file created after yarn install"""
+    return [repo / "node_modules/.yarn-integrity"]
