499ecbc1d34781bb36813ee4a9c589de82cacd0d
tony fast <tony.fast@gmail.com>
2022-01-27 12:07:40 -0800

    replace sphinx w jb, use nox for building

--- noxfile.py
+++ noxfile.py
@@ -5,3 +5,8 @@ import nox
 def build(session):
     session.install("doit")
     session.run("doit", *session.posargs)
+
+@nox.session(reuse_venv=True)
+def docs(session):
+    session.install("jupyter-book")
+    session.run("jb", "build", ".", "--toc", "docs/_toc.yml", "--config", "docs/_config.yml"), *session.posargs
