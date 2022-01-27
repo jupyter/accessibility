a2bb3274eeb973d99526e30a169f198d1f17ac4e
Gabriel Fouasnon <gabriel@fouasnon.com>
2022-01-27 10:21:13 -0600

    test notebook after running all cells

--- tests/retrolab/test/a11y.test.ts
+++ tests/retrolab/test/a11y.test.ts
@@ -9,7 +9,7 @@ const LORENZ = "Lorenz.ipynb";
 test.describe.parallel("accessibility checks", () => {
   test.beforeEach(async ({ page, tmpPath }) => {
     await page.contents.uploadFile(
-      __dirname + '/' + LORENZ,
+      __dirname + "/" + LORENZ,
       `${tmpPath}/${LORENZ}`
     );
   });
@@ -19,7 +19,10 @@ test.describe.parallel("accessibility checks", () => {
    * to run analysis on page
    * @see https://github.com/abhinaba-ghosh/axe-playwright
    */
-  test("Check Lorenz notebook accessibility before running all cells", async ({ page, tmpPath }, testInfo) => {
+  test("Check Lorenz notebook accessibility before running all cells", async ({
+    page,
+    tmpPath,
+  }, testInfo) => {
     await page.goto(`notebooks/${tmpPath}/${LORENZ}`);
 
     // inject the axe-core runtime into the page under test
@@ -69,61 +72,66 @@ test.describe.parallel("accessibility checks", () => {
     await expect(violations).toEqual([]);
   });
 
-  /**
-   * In this test we use the Axe accessibility testing engine
-   * to run analysis on page
-   * @see https://github.com/abhinaba-ghosh/axe-playwright
-   */
-  // test("Check Lorenz notebook accessibility after running all cells", async ({ page, tmpPath }, testInfo) => {
-  //   await page.goto(`notebooks/${tmpPath}/${LORENZ}`);
+  test("Check Lorenz notebook accessibility after running all cells", async ({
+    page,
+    tmpPath,
+  }, testInfo) => {
+    await page.goto(`notebooks/${tmpPath}/${LORENZ}`);
 
-  //   await page.click('.lm-MenuBar-itemLabel:has-text("Run")');
-  //   await page.click('.lm-Menu-itemLabel:has-text("Run All Cells")');
+    await page.click('.lm-MenuBar-itemLabel:has-text("Run")');
+    await page.click('.lm-Menu-itemLabel:has-text("Run All Cells")');
+    await page.waitForSelector('pre:has-text("Okay all done running 4")');
+    await page.evaluate(() => {
+      const el = document.querySelector(".jp-NotebookPanel-notebook");
+      const pres = el.querySelectorAll("pre");
+      const lastPre = pres[pres.length - 1];
+      lastPre.scrollIntoView();
+    });
 
-  //   // inject the axe-core runtime into the page under test
-  //   await injectAxe(page as any);
+    // inject the axe-core runtime into the page under test
+    await injectAxe(page as any);
 
-  //   const violations = await getViolations(page as any, null, {
-  //     detailedReport: true,
-  //     detailedReportOptions: { html: true },
-  //     axeOptions: {
-  //       runOnly: {
-  //         type: "tag",
-  //         values: ["wcag2a", "best-practice"], // all tags and standards listed here: https://www.deque.com/axe/core-documentation/api-documentation/#axe-core-tags
-  //       },
-  //     },
-  //   });
+    const violations = await getViolations(page as any, null, {
+      detailedReport: true,
+      detailedReportOptions: { html: true },
+      axeOptions: {
+        runOnly: {
+          type: "tag",
+          values: ["wcag2a", "best-practice"], // all tags and standards listed here: https://www.deque.com/axe/core-documentation/api-documentation/#axe-core-tags
+        },
+      },
+    });
 
-  //   if (violations.length > 0) {
-  //     const axeReportPath = testInfo.outputPath("axe-report.html");
-  //     const html = createHtmlReport({
-  //       results: {
-  //         violations: violations,
-  //       },
-  //       options: {
-  //         doNotCreateReportFile: true,
-  //       },
-  //     });
-  //     await fs.promises.writeFile(axeReportPath, html);
-  //     testInfo.attachments.push({
-  //       name: "axe-html-report",
-  //       path: axeReportPath,
-  //       contentType: "text/html",
-  //     });
-  //   }
+    if (violations.length > 0) {
+      const axeReportPath = testInfo.outputPath("axe-report.html");
+      const html = createHtmlReport({
+        results: {
+          violations: violations,
+        },
+        options: {
+          doNotCreateReportFile: true,
+        },
+      });
+      await fs.promises.writeFile(axeReportPath, html);
+      testInfo.attachments.push({
+        name: "axe-html-report",
+        path: axeReportPath,
+        contentType: "text/html",
+      });
+    }
 
-  //   const accessibilityTree = await page.accessibility.snapshot();
-  //   const treePath = testInfo.outputPath("accessibility-tree.json");
-  //   await fs.promises.writeFile(
-  //     treePath,
-  //     JSON.stringify(accessibilityTree, null, 2)
-  //   );
-  //   testInfo.attachments.push({
-  //     name: "Accessibility Tree",
-  //     path: treePath,
-  //     contentType: "application/json",
-  //   });
+    const accessibilityTree = await page.accessibility.snapshot();
+    const treePath = testInfo.outputPath("accessibility-tree.json");
+    await fs.promises.writeFile(
+      treePath,
+      JSON.stringify(accessibilityTree, null, 2)
+    );
+    testInfo.attachments.push({
+      name: "Accessibility Tree",
+      path: treePath,
+      contentType: "application/json",
+    });
 
-  //   await expect(violations).toEqual([]);
-  // });
+    await expect(violations).toEqual([]);
+  });
 });
