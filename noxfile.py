2a539e5a88f04a354520c9a3c532f7cbb9d22de3
tony fast <tony.fast@gmail.com>
2022-01-28 11:06:37 -0800

    use doit tasks for building docs and use sphinx-build

--- noxfile.py
+++ noxfile.py
@@ -1,12 +1,46 @@
-import nox
+
+from contextlib import suppress
+from pathlib import Path
+
+TOC = Path("docs", "_toc.yml")
+CONFIG = Path("docs", "_config.yml")
+CONF = Path("docs", "conf.py")
+
+DOIT_CONFIG = dict(verbosity=2)
 
 
-@nox.session(venv_backend="conda")
-def build(session):
-    session.install("doit")
-    session.run("doit", *session.posargs)
+def task_docs():
+    def config():
+        with suppress(SystemExit):
+            CmdAction(F"jb config sphinx --toc {TOC.absolute()} --config {CONFIG.absolute()} .".split(), shell=False)
+    from doit.action import CmdAction
+    
+    yield dict(
+        name="configure",
+        file_dep=[TOC, CONFIG],
+        actions=[
+          config  
+        ],
+        targets=[CONF],
+        clean=True,
+    )
+    yield dict(
+        name="build",
+        actions=[F"sphinx-build -c {CONF.parent} . _build/html"],
+        targets=["_build/html/index.html"],
+        uptodate=[False]
+    )
+try:
+    import nox
+except ModuleNotFoundError:
+    pass
+else:
+    @nox.session(venv_backend="conda")
+    def build(session):
+        session.install("doit")
+        session.run("doit", *session.posargs)
 
-@nox.session(reuse_venv=True)
-def docs(session):
-    session.install("jupyter-book")
-    session.run("jb", "build", ".", "--toc", "docs/_toc.yml", "--config", "docs/_config.yml"), *session.posargs
+    @nox.session(reuse_venv=True)
+    def docs(session):
+        session.install("jupyter-book", "doit")
+        session.run("doit", "-f", "noxfile.py", *session.posargs), 
