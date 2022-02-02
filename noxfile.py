6a978c30e9cf374f29311586dbcf92355f472934
tony fast <tony.fast@gmail.com>
2022-02-01 21:38:08 -0800

    use the CONF variable

--- noxfile.py
+++ noxfile.py
@@ -1,41 +1,45 @@
-
 from contextlib import suppress
 from pathlib import Path
 
 TOC = Path("docs", "_toc.yml")
 CONFIG = Path("docs", "_config.yml")
-CONF = Path("docs", "conf.py")
+CONF = Path("conf.py")
 
 DOIT_CONFIG = dict(verbosity=2)
 
 
 def task_docs():
     def config():
-        cmd = CmdAction(F"jb config sphinx --toc {TOC.absolute()} --config {CONFIG.absolute()} .".split(), shell=False)
+        cmd = CmdAction(
+            f"jb config sphinx --toc {TOC.absolute()} --config {CONFIG.absolute()} .".split(),
+            shell=False,
+        )
         with suppress(SystemExit):
             cmd.execute()
+
     from doit.action import CmdAction
-    
+
     yield dict(
         name="configure",
         file_dep=[TOC, CONFIG],
-        actions=[
-          config, "mv docs/conf.py conf.py"
-        ],
-        targets=["conf.py"],
+        actions=[config, f"mv {TOC.parent / CONF} {CONF}"],
+        targets=[CONF],
         clean=True,
     )
     yield dict(
         name="build",
-        actions=[F"sphinx-build . _build/html"],
+        actions=[f"sphinx-build . _build/html"],
         targets=["_build/html/index.html"],
-        uptodate=[False]
+        uptodate=[False],
     )
+
+
 try:
     import nox
 except ModuleNotFoundError:
     pass
 else:
+
     @nox.session(venv_backend="conda")
     def build(session):
         session.install("doit")
@@ -44,4 +48,4 @@ else:
     @nox.session(reuse_venv=True)
     def docs(session):
         session.install("doit", "-rdocs/requirements.txt")
-        session.run("doit", "-f", "noxfile.py", *session.posargs), 
+        session.run("doit", "-f", "noxfile.py", *session.posargs),
