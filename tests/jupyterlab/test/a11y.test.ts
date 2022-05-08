3faf65a05d71ac7854f73e39bc5873eb329275c0
Gabriel Fouasnon <gabriel@fouasnon.com>
2022-05-07 21:07:08 -0500

    reformat a11y.test.ts

--- tests/jupyterlab/test/a11y.test.ts
+++ tests/jupyterlab/test/a11y.test.ts
@@ -3,7 +3,7 @@ import { injectAxe, getViolations } from "axe-playwright";
 import { test } from "@jupyterlab/galata";
 import { createHtmlReport } from "axe-html-reporter";
 import fs from "fs";
-import * as path from 'path';
+import * as path from "path";
 
 const LORENZ = "Lorenz.ipynb";
 const AXE_CONFIG = {
@@ -15,14 +15,13 @@ const AXE_CONFIG = {
       values: ["wcag2a", "best-practice"], // all tags and standards listed here: https://www.deque.com/axe/core-documentation/api-documentation/#axe-core-tags
     },
   },
-}
+};
 
 async function axe(page, testInfo) {
   await injectAxe(page as any);
 
-
-  const violations = await getViolations(page as any, null, AXE_CONFIG);
-  const axeResultsJsonFilepath = testInfo.outputPath("axe-results.json")
+  const violations = await getViolations(page as any, null, AXE_CONFIG as any);
+  const axeResultsJsonFilepath = testInfo.outputPath("axe-results.json");
   const axeResultsJson = JSON.stringify(violations);
   await fs.promises.writeFile(axeResultsJsonFilepath, axeResultsJson);
   testInfo.attachments.push({
@@ -61,9 +60,13 @@ async function axe(page, testInfo) {
     contentType: "application/json",
   });
   return violations;
-
 }
 
+/**
+ * In these tests we use the Axe accessibility testing engine
+ * to run analysis on page
+ * @see https://github.com/abhinaba-ghosh/axe-playwright
+ */
 test.describe("accessibility checks", () => {
   test.beforeEach(async ({ page, tmpPath }) => {
     await page.contents.uploadDirectory(
@@ -72,42 +75,82 @@ test.describe("accessibility checks", () => {
     );
   });
 
-  /**
-   * In this test we use the Axe accessibility testing engine
-   * to run analysis on page
-   * @see https://github.com/abhinaba-ghosh/axe-playwright
-   */
-  for (const nb of [LORENZ]) {
-    for (const exec of [false, true]) {
-      test(`notebook a11y ${nb} execute ${exec}`, async ({
-        page,
-        tmpPath,
-      }, testInfo) => {
-        await page.goto(`tree/${tmpPath}/${LORENZ}`);
-        // the following helper only works for jupyterlab not retrolab
-        // await page.notebook.openByPath(`${tmpPath}/${nb}`);
+  function makeTest({ nb, exec }) {
+    return async function doTest({ page, tmpPath }, testInfo) {
+      await page.goto(`tree/${tmpPath}/${LORENZ}`);
+      // the following helper only works for jupyterlab not retrolab
+      // await page.notebook.openByPath(`${tmpPath}/${nb}`);
 
-        if (exec) {
-          await page.notebook.run()
+      if (exec) {
+        await page.notebook.run();
+      }
+
+      // playwright records the browser window while running, and then uploads
+      // the video with the test results to help with debugging. So here we
+      // scroll to the bottom of the notebook so that if somebody needs to
+      // watch the video, they can see the whole notebook from beginning to
+      // end.
+      await page.evaluate(() => {
+        for (let i = 0; i < document.body.scrollHeight; i += 100) {
+          window.scrollTo(0, i);
         }
-
-        // playwright records the browser window while running, and then uploads
-        // the video with the test results to help with debugging. So here we
-        // scroll to the bottom of the notebook so that if somebody needs to
-        // watch the video, they can see the whole notebook from beginning to
-        // end.
-        await page.evaluate(() => {
-          const el = document.querySelector(".jp-NotebookPanel-notebook");
-          const pres = el.querySelectorAll("pre");
-          const lastPre = pres[pres.length - 1];
-          lastPre.scrollIntoView();
-        });
-
-        const violations = await axe(page, testInfo)
-
-        await expect(violations).toEqual([]);
       });
-    }
+
+      const violations = await axe(page, testInfo);
+
+      await expect(violations).toEqual([]);
+    };
   }
 
+  test("notebook a11y LORENZ execute false", async ({
+    page,
+    tmpPath,
+  }, testInfo) => {
+    await page.notebook.openByPath(`${tmpPath}/${LORENZ}`);
+
+    if (false) {
+      await page.notebook.run();
+    }
+
+    // playwright records the browser window while running, and then uploads
+    // the video with the test results to help with debugging. So here we
+    // scroll to the bottom of the notebook so that if somebody needs to
+    // watch the video, they can see the whole notebook from beginning to
+    // end.
+    await page.evaluate(() => {
+      for (let i = 0; i < document.body.scrollHeight; i += 100) {
+        window.scrollTo(0, i);
+      }
+    });
+
+    const violations = await axe(page, testInfo);
+
+    await expect(violations).toEqual([]);
+  });
+
+  test("notebook a11y LORENZ execute true", async ({
+    page,
+    tmpPath,
+  }, testInfo) => {
+    await page.notebook.openByPath(`${tmpPath}/${LORENZ}`);
+
+    if (true) {
+      await page.notebook.run();
+    }
+
+    // playwright records the browser window while running, and then uploads
+    // the video with the test results to help with debugging. So here we
+    // scroll to the bottom of the notebook so that if somebody needs to
+    // watch the video, they can see the whole notebook from beginning to
+    // end.
+    await page.evaluate(() => {
+      for (let i = 0; i < document.body.scrollHeight; i += 100) {
+        window.scrollTo(0, i);
+      }
+    });
+
+    const violations = await axe(page, testInfo);
+
+    await expect(violations).toEqual([]);
+  });
 });
