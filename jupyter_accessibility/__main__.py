962597e2a279d1f513568a4f2a007139ddb8af16
Gabriel Fouasnon <gabriel@fouasnon.com>
2022-07-21 17:18:03 +0200

    Revert "Merge pull request #90 from Quansight-Labs/doit-pw"
    This reverts commit 055699477e14d0a14fec40aa6d6696a93e6e386a, reversing
    changes made to ed2254d87d7bb922644134ac95bb66d4965b7dfc.

--- jupyter_accessibility/__main__.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) Jupyter Development Team.
-# Distributed under the terms of the Modified BSD License.
-
-from .dodo import Main
-
-main = Main.run
-
-if __name__ == "__main__":
-    main()
