29eb5aa95a19f30c93061161e42593e4e1078a8a
Tania Allard <taniar.allard@gmail.com>
2022-08-08 18:22:39 +0100

    :building_construction: Modify nox file

--- noxfile.py
+++ noxfile.py
@@ -6,33 +6,43 @@
 
 import nox
 
+# ensure we reuse environments instead of creating new ones each time
 nox.options.reuse_existing_virtualenvs = True
-BUILD_COMMAND = ["-b", "html", "docs", "docs/_build/html"]
+nox.options.default_venv_backend = "conda"
+
+DOCS_BUILD_DIR = "docs/_build/html"
+BUILD_COMMAND = ["-b", "html", "docs", DOCS_BUILD_DIR]
 
 
 def install_deps(session):
-    """Install dependencies for the project. Leveraging conda inside nox so that we can reuse the environment."""
-    # Manually installing this because conda is a bit wonky w/ nox
+    """Install dependencies for the project - using conda. Leveraging conda inside nox so that we can reuse the environment."""
+
+    # https://nox.thea.codes/en/stable/tutorial.html#testing-with-conda
     session.conda_install("--channel=conda-forge", "python=3.9")
-    session.install("-r", "docs/requirements.txt")
+    session.conda_install("--file", "docs/requirements.txt")
 
 
 @nox.session(venv_backend="conda")
 def docs(session):
     """Install the necessary dependencies and build the docs.
     Recommended for CI - or if you do not need a realtime preview of the docs."""
+
     install_deps(session)
-    session.run("jupyterbook", *BUILD_COMMAND)
+    session.run("sphinx-build", *BUILD_COMMAND)
 
 
 @nox.session(name="docs-live", venv_backend="conda")
 def docs_live(session):
     """Install necessary dependencies, buid the docs and use livereload.
-    Recommended for local development - livereload allows you to see the changes in rea-time."""
+    Recommended for local development - livereload allows you to see the changes in real-time."""
+
     install_deps(session)
 
+    session.run("jupyter-book", "config", "sphinx", "docs")
     cmd = ["sphinx-autobuild"]
+
     for path in ["*/_build/*", "*/tmp/*"]:
         cmd.extend(["--ignore", path])
+
     cmd.extend(BUILD_COMMAND)
     session.run(*cmd)
